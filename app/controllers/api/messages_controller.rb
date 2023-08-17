62256f928e874971a08cbebc4cc69ee808f03ca4
adeola003 <khadaf3@gmail.com>
2023-08-17 14:42:17 +0100

    api working

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,2 +1,7 @@
 class Api::MessagesController < ApplicationController
+  def random_greeting
+    random_message = Message.order('RANDOM()').first
+    render json: { greeting: random_message.content }
+  end
+
 end
